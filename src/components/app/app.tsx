ab51d51f2b178b7783fdc33485fce70995823bbf
LevDiakov <381208dl@gmail.com>
2024-07-11 18:19:40 -0300

    Добавит маршрутизацию с React Router 6 и компонент layout.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,9 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import Layout from '../layout/layout';
 
 type AppProps = {
   numberOfRentalOffers: number;
@@ -6,7 +11,16 @@ type AppProps = {
 
 function App({numberOfRentalOffers}: AppProps): JSX.Element {
   return (
-    <MainPage numberOfRentalOffers={numberOfRentalOffers} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<MainPage numberOfRentalOffers={numberOfRentalOffers} />} />
+          <Route path='offer-page' element={<OfferPage />} />
+          <Route path='login-page' element={<LoginPage />} />
+          <Route path='favorites-page' element={<FavoritesPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
