502e4ad6b8e6a09df201c43128617ca06b995c1e
LevDiakov <381208dl@gmail.com>
2024-07-19 20:48:28 -0300

    Добавит моки для полных предложений по аренде и отзывов

--- src/types/types.ts
+++ src/types/types.ts
@@ -26,3 +26,34 @@ export type Offer = {
   rating: number;
 }
 
+export type Host = {
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+}
+
+export type FullOffer = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  city: City;
+  location: Location;
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+  description: string;
+  bedrooms: number;
+  goods: string[];
+  host: Host;
+  images: string[];
+  maxAdults: number;
+}
+
+export type Comment = {
+  id: string;
+  date: string;
+  user: Host;
+  comment: string;
+  rating: number;
+}
